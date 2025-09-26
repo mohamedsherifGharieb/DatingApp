994e47a410e499862aa5e3f3e4d5d65fb7ccb8a6
mohamedsherifGharieb <icold689@yahoo.com>
2025-09-26 05:54:11 +0300

    init line modified for unnessecary call

--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,21 +1,18 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
-import { Observable, of, tap } from 'rxjs';
-import { LikesServices } from './likes-services';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InitService {
   private accountService = inject(AccountService);
-  private likeService = inject(LikesServices);
 
   init() {
     return this.accountService.refreshToken().pipe(
       tap(user => {
-        if (user)
-          this.accountService.currentUser.set(user);
-        this.likeService.getLikesIds();
+      if (user)
+        this.accountService.setCurrentUser(user);
         this.accountService.startRefreshIntervel();
       })
     )
