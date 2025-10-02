4e2b998233b2ae5316154013b40cfc6d78b8f266
mohamedsherifGharieb <icold689@yahoo.com>
2025-10-02 07:01:08 +0300

    small register button adjustment

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -1,6 +1,7 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
 import { Register } from "../account/register/register";
 import { User } from '../../Types/user';
+import { AccountService } from '../../core/services/account-service';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ import { User } from '../../Types/user';
 })
 export class Home {
   protected registerMode = signal(false);
+  protected accountService = inject(AccountService);
 
   showRegisterMode(value: boolean){
     this.registerMode.set(value);
